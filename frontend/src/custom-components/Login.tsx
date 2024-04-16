477167fb93d1da959ad4ed08e6716835573d4fda
reo113 <roberteliortiz@gmail.com>
2024-04-16 18:56:03 -0400

    added auth0

---
+++ frontend/src/custom-components/Login.tsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
+function LoginButton() {
+    const {
+      isAuthenticated,
+      loginWithRedirect,
+    } = useAuth0();
+  
+    return !isAuthenticated && (
+      <button onClick={loginWithRedirect}>Log in</button>
+    );
+  }
+  
+  export default LoginButton;
