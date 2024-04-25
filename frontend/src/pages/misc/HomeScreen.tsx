fd38f5a230f07fcd280649814ce719f350d23ae7
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-25 14:48:38 -0400

    move homepage to misc pages

--- frontend/src/pages/misc/HomeScreen.tsx
+++ frontend/src/pages/misc/HomeScreen.tsx
@@ -1,14 +1,14 @@
-import DefintionGame from "../games/definition-game/DefinitionGame";
+import DefintionGame from "../games/components/DefinitionGame";
 const HomeScreen = () => {
   return (
     <div>
       <div className="container">
         <h1>Home Screen</h1>
         <p>Welcome to the Home Screen</p>
-        <DefintionGame/>
+        <DefintionGame />
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
