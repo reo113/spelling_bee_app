07c4ef6d1aa43d9e8e5af0d735d5ed69726eaa33
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-25 13:24:03 -0400

    move browser router from main.tsx to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import HomeScreen from "./components/custom/HomeScreen";
-import { Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // import GameScreen from "./components/custom/games/DefinitionGame";
 // import AudioGameScreen from "./AudioGame";
 function App() {
-  return (
-   <div>
-    <Routes>
-    <Route path="/" element={<HomeScreen/>}/>
-      </Routes>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeScreen />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
