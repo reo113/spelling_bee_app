cd04d727314a72f97375ccf4efda37cd7669cfda
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-28 16:39:05 -0400

    export middleware in seperate folder

---
+++ backend/src/middleware/log.js
@@ -0,0 +1,9 @@
+const log = (req, res, next) => {
+  console.log(`Request: ${req.method} ${req.originalUrl}`);
+  res.on("finish", () => {
+    console.log(`Response Status: ${res.statusCode}`);
+  });
+  next();
+};
+
+module.exports = log;
