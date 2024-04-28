3c798d836e6b234e47304ab028674fdb015bd3c1
reo113 <roberteliortiz@gmail.com>
2024-04-28 15:43:43 -0400

    created gamehub

--- backend/routes/definition.js
+++ backend/routes/definition.js
@@ -1,53 +1,55 @@
 // const apiKey = "v200pgdwucwqdtqnp2dll678gosivzb0761lllr8w0ql2na70";
 // const urlr = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=noun-plural&limit=5&api_key=${apiKey}`;
 
-const { PrismaClient } = require('@prisma/client');
-const _ = require('lodash');
+const { PrismaClient } = require("@prisma/client");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const prisma = new PrismaClient();
 
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const wordsWithDefinitions = await prisma.dictionary.findMany({
-      take: 50,
-      select: { word: true, definition: true }
+    let wordsWithDefinitions = await prisma.dictionary.findMany({
+      take: 100,
+      select: { word: true, definition: true },
     });
 
     // split the words into correct and incorrect answers
+    wordsWithDefinitions = _.shuffle(wordsWithDefinitions);
     const correctAnswers = wordsWithDefinitions.slice(0, 10);
     let incorrectPool = wordsWithDefinitions.slice(10);
 
-    const result = correctAnswers.map(word => {
-      //shuffle and pick two incorrect definitions
+    const result = correctAnswers.map((word) => {
+      //shuffle and pick two incorrect words
       const shuffledIncorrect = _.shuffle(incorrectPool).slice(0, 2);
-      // remove the already used definitions from the pool
-      incorrectPool = incorrectPool.filter(def => !shuffledIncorrect.includes(def));
+      // remove the already used words from the pool
+      incorrectPool = incorrectPool.filter(
+        (def) => !shuffledIncorrect.includes(def)
+      );
       const finalWords = _.shuffle([
         word.word,
         shuffledIncorrect[0].word,
-        shuffledIncorrect[1].word
+        shuffledIncorrect[1].word,
       ]);
 
       return {
         answer: word,
-        words: finalWords
+        words: finalWords,
       };
     });
     res.json(result);
   } catch (error) {
-    console.error('Error fetching words and definitions:', error);
-    res.status(500).send('Server Error');
+    console.error("Error fetching words and definitions:", error);
+    res.status(500).send("Server Error");
   }
 });
 
 // Assuming similar structure for other game types
-router.get('/audio', async (req, res) => {
+router.get("/audio", async (req, res) => {
   // Your logic for handling 'audio' game type
 });
 
-router.get('/sentence', async (req, res) => {
+router.get("/sentence", async (req, res) => {
   // Your logic for handling 'sentence' game type
 });
 module.exports = router;
