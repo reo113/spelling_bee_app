b8819dec9fdc235dae90775fbf47591d26944dd0
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-29 00:34:56 -0400

    add user route for testing auth required endpoint

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 
 const auth = require("./auth/auth.routes");
+const user = require("./user/user.routes");
 const definition = require("./definition/definition.routes");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    message: "API - 👋",
+    message: "API",
   });
 });
 
 router.use("/auth", auth);
+router.use("/user", user);
 router.use("/definition", definition);
 
 module.exports = router;
