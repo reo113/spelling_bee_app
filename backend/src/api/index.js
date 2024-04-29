de57b2d4b95302edcc78f2e44b0b2b1317f69855
reo113 <roberteliortiz@gmail.com>
2024-04-29 18:52:55 -0400

    added the new game routes

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const auth = require("./auth/auth.routes");
 const definition = require("./definition/definition.routes");
-
+const audio = require("./audio/audio.routes");
+const sentence = require("./sentence/sentence.routes");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -13,5 +13,7 @@ router.get("/", (req, res) => {
 
 router.use("/auth", auth);
 router.use("/definition", definition);
+router.use("/audio", audio);
+router.use("/sentence", sentence);
 
 module.exports = router;
