f4eb170cb8585bca83fc64dcc0794f4fd63a3d0f
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-30 14:11:23 -0400

    add find user by id function

--- backend/src/api/user/user.services.js
+++ backend/src/api/user/user.services.js
@@ -1,6 +1,14 @@
 const bcrypt = require("bcryptjs");
 const { db } = require("../../utils/db");
 
+function findUserById(id) {
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
 function findUserByEmail(email) {
   return db.user.findUnique({
     where: {
@@ -13,14 +21,6 @@ function createUserByEmailAndPassword(user) {
   user.password = bcrypt.hashSync(user.password, 12);
   return db.user.create({
     data: user,
-  });
-}
-
-function findUserById(id) {
-  return db.user.findUnique({
-    where: {
-      id,
-    },
   });
 }
 
