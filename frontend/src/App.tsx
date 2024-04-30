a2571c3e951e0a69e62aed03f81d99bf9ef0ac03
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-30 14:14:45 -0400

    add login and register routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
+import Login from "@/pages/auth/components/Login";
+import Register from "@/pages/auth/components/Register";
 import HomeScreen from "@/pages/misc/HomeScreen";
+import AuthProvider from "@/contexts/AuthContext";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // import GameScreen from "./components/custom/games/DefinitionGame";
 // import AudioGameScreen from "./AudioGame";
@@ -8,9 +11,21 @@ function App() {
       path: "/",
       element: <HomeScreen />,
     },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
