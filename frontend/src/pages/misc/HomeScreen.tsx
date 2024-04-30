a9087a3e109a3fd8033a51d7027f6b108e9f37c5
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-04-30 14:14:35 -0400

    add login and logout button for testing

--- frontend/src/pages/misc/HomeScreen.tsx
+++ frontend/src/pages/misc/HomeScreen.tsx
@@ -1,12 +1,38 @@
 import StartGame from "@/games/components/StartGame";
+import { redirect } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { AuthContext } from "@/contexts/AuthContext";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
 
 const HomeScreen = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout();
+    redirect("/");
+  };
   return (
     <div>
       <div className="container">
         <h1>Home Screen</h1>
         <p>Welcome to the Home Screen</p>
         <StartGame />
+        {currentUser ? (
+          <Button
+            onClick={handleLogout}
+            className="mx-4 transition-all duration-300 hover:text-accent dark:text-white dark:hover:text-accent"
+          >
+            <span className="ml-2">Log Out</span>
+          </Button>
+        ) : (
+          <Link
+            to="/login"
+            className="mx-4 transition-all duration-300 hover:text-accent dark:text-white dark:hover:text-accent"
+          >
+            <span className="ml-2">Log In</span>
+          </Link>
+        )}
       </div>
     </div>
   );
