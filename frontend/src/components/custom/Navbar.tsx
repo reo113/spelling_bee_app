abc31403f0e859a02f55a53eaa7b5918ef63db4e
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-05-03 23:06:32 -0400

    add link to logo and name

--- frontend/src/components/custom/Navbar.tsx
+++ frontend/src/components/custom/Navbar.tsx
@@ -20,16 +20,18 @@ export default function Navbar() {
   return (
     <div className="sm:px-18 md:px-26 mx-auto flex w-full items-center justify-between bg-white bg-none px-4 py-6 shadow-md lg:px-48 xl:px-64">
       {/* Image and Spelling Bee Text Group */}
-      <div className="flex items-center">
-        <img
-          src="/logo.svg"
-          alt="Spelling Bee Logo"
-          className="h-8 w-16 sm:h-16 sm:w-32"
-        />
-        <span className="text-xl font-bold sm:text-2xl md:text-3xl">
-          Spelling Bee App
-        </span>
-      </div>
+      <Link to="/">
+        <div className="flex items-center">
+          <img
+            src="/logo.svg"
+            alt="Spelling Bee Logo"
+            className="h-8 w-16 sm:h-16 sm:w-32"
+          />
+          <span className="text-xl font-bold sm:text-2xl md:text-3xl">
+            Spelling Bee App
+          </span>
+        </div>
+      </Link>
       <div className="flex items-center justify-center gap-x-4">
         {/* Language Selector Dropdown */}
         <div className="hidden sm:block">
