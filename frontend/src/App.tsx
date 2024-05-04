be2b91ce4956234bef219a36f1401972266ccaa5
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-05-04 17:19:38 -0400

    initialize i18next provider and add translations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,26 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import common_en from "./translations/en/common.json";
+import common_es from "./translations/es/common.json";
+
 function App() {
+  i18next.init({
+    interpolation: { escapeValue: false },
+    lng: "en",
+    resources: {
+      en: {
+        common: common_en,
+      },
+      es: {
+        common: common_es,
+      },
+    },
+  });
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -56,7 +75,9 @@ function App() {
 
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <I18nextProvider i18n={i18next}>
+        <RouterProvider router={router} />
+      </I18nextProvider>
     </AuthProvider>
   );
 }
