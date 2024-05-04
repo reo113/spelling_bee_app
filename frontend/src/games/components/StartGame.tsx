98dad89a2d5798eb9b0d211f5f690f8a8b65a185
reo113 <roberteliortiz@gmail.com>
2024-05-04 19:17:42 -0400

    created spanish dictionary, populated database. user able to play spanish dictinary game

--- frontend/src/games/components/StartGame.tsx
+++ frontend/src/games/components/StartGame.tsx
@@ -1,27 +1,32 @@
-import { useCallback, useEffect, useState,useContext } from "react";
+import { useCallback, useEffect, useState, useContext } from "react";
 import GameFactory from "../factory/gameFactory";
 import GameWrapper from "../wrapper/GameWrapper";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
 import Loading from "@/components/custom/Loading";
 import { AuthContext } from "@/contexts/AuthContext";
+import { useTranslation } from "react-i18next";
+
 const StartGame = () => {
   const [game, setGame] = useState(null);
   const [loading, setLoading] = useState(true);
   const { gameType } = useParams();
   const { currentUser } = useContext(AuthContext);
-
+  const { i18n } = useTranslation();
   // Define fetchData using useCallback
   let userId = null;
-  if(currentUser){
+  if (currentUser) {
     userId = currentUser.id;
   }
   const fetchData = useCallback((type) => {
-  console.log("fetching",type);
+    console.log("fetching", type);
     setLoading(true);
     axios
-      .post(`/api/v1/${type}`,{userId:userId, type:type})
+      .post(`/api/v1/${type}`, {
+        userId: userId,
+        type: type,
+        languageCode: i18n.language,
+      })
       .then((response) => {
         const data = response.data;
         const newGame = GameFactory.createGame(type, data);
