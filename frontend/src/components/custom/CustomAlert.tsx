c2899b90c014084995637d84bc73b10f98385ec7
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-05-06 15:56:02 -0400

    add translations for alert and warning messages

--- frontend/src/components/custom/CustomAlert.tsx
+++ frontend/src/components/custom/CustomAlert.tsx
@@ -2,14 +2,18 @@ import { CheckCheck } from "lucide-react";
 
 import { Alert, AlertDescription } from "@/components/ui/alert";
 
+import { useTranslation } from "react-i18next";
+
 export default function CustomAlert() {
+  const { t } = useTranslation("common");
+
   return (
     <Alert
       variant="default"
       className="border-green-500 text-green-500 [&>svg]:text-green-500"
     >
       <CheckCheck className="h-4 w-4" />
-      <AlertDescription>Supported in English and Spanish.</AlertDescription>
+      <AlertDescription>{t("games.alert")}</AlertDescription>
     </Alert>
   );
 }
--- frontend/src/components/custom/CustomWarning.tsx
+++ frontend/src/components/custom/CustomWarning.tsx
@@ -2,14 +2,18 @@ import { TriangleAlert } from "lucide-react";
 
 import { Alert, AlertDescription } from "@/components/ui/alert";
 
+import { useTranslation } from "react-i18next";
+
 export default function CustomWarning() {
+  const { t } = useTranslation("common");
+
   return (
     <Alert
       variant="default"
       className="border-yellow-500 text-yellow-500 [&>svg]:text-yellow-500"
     >
       <TriangleAlert className="h-4 w-4" />
-      <AlertDescription>Only supported in English.</AlertDescription>
+      <AlertDescription>{t("games.warning")}</AlertDescription>
     </Alert>
   );
 }
