64618c356d1cd064177d97599675351878add6c0
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-05-05 21:32:33 -0400

    remove props

--- frontend/src/games/components/Header.tsx
+++ frontend/src/games/components/Header.tsx
@@ -2,17 +2,13 @@ import { Progress } from "@/components/ui/progress";
 import { X } from "lucide-react";
 import { Link } from "react-router-dom";
 
-const Header = ({ lives, userId, index }) => {
+const Header = ({ index }) => {
   return (
-    <header
-      className="mx-auto flex w-full max-w-[1140px] items-center
-    justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]"
-    >
+    <header className="mx-auto flex w-full items-center justify-between gap-x-7 pt-[20px] lg:pt-[50px]">
       <Link to="/games">
         <X className="cursor-pointer text-slate-500 transition hover:text-bee" />
       </Link>
       <Progress value={index} />
-      <div className="font bold flex items-center text-rose-500"></div>
     </header>
   );
 };
