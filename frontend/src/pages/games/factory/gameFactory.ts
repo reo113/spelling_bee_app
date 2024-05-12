8861e52fa7b779282a644d6f7a54c5d7b9f7c37d
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-05-12 19:49:05 -0400

    rename and export all three game classes

--- frontend/src/pages/games/factory/gameFactory.ts
+++ frontend/src/pages/games/factory/gameFactory.ts
@@ -1,19 +1,21 @@
-import DefinitionGame from './gameClass';
-import {AudioGame} from './gameClass';
-import {SentenceGame} from './gameClass';
+import {
+  DefinitionGameClass,
+  AudioGameClass,
+  SentenceGameClass,
+} from "./gameClass";
 
 class GameFactory {
-    static createGame(type: string, data) {
-        switch (type) {
-            case 'definition':
-                return new DefinitionGame(data);
-            case 'audio':
-                return new AudioGame(data);
-            case 'sentence':
-                return new SentenceGame(data);
-            default:
-                throw new Error('Unknown game type');
-        }
+  static createGame(type: string, data) {
+    switch (type) {
+      case "definition":
+        return new DefinitionGameClass(data);
+      case "audio":
+        return new AudioGameClass(data);
+      case "sentence":
+        return new SentenceGameClass(data);
+      default:
+        throw new Error("Unknown game type");
     }
+  }
 }
 export default GameFactory;
